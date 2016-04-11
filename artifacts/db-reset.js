623b1db7a5ffd5112afd5b2f3df25ec7e515e447
Johnathan Bui <bui@Johnathans-MacBook-Pro.local>
2016-04-11 06:42:12 -0400

    changed name

--- artifacts/db-reset.js
+++ artifacts/db-reset.js
@@ -13,8 +13,8 @@ var USERS_TO_INSERT = [
     {
         "_id": 1,
         "userName": "admin",
-        "firstName": "Node Goat",
-        "lastName": "Admin",
+        "firstName": "Admin",
+        "lastName": "Account",
         "password": "Admin_123",
         //"password" : "$2a$10$8Zo/1e8KM8QzqOKqbDlYlONBOzukWXrM.IiyzqHRYDXqwB3gzDsba", // Admin_123
         "isAdmin": true
@@ -22,15 +22,15 @@ var USERS_TO_INSERT = [
         "_id": 2,
         "userName": "user1",
         "firstName": "John",
-        "lastName": "Doe",
+        "lastName": "Bui",
         "benefitStartDate": "2030-01-10",
         "password": "User1_123"
             // "password" : "$2a$10$RNFhiNmt2TTpVO9cqZElb.LQM9e1mzDoggEHufLjAnAKImc6FNE86",// User1_123
     }, {
         "_id": 3,
         "userName": "user2",
-        "firstName": "Will",
-        "lastName": "Smith",
+        "firstName": "Donald",
+        "lastName": "Trump",
         "benefitStartDate": "2025-11-30",
         "password": "User2_123"
             //"password" : "$2a$10$Tlx2cNv15M0Aia7wyItjsepeA8Y6PyBYaNdQqvpxkIUlcONf1ZHyq", // User2_123
